2d3729550593e8b25711774d8cfaddee06120d2f
14se059 <14se059@13se991.C213.seto-private>
2017-07-12 14:05:06 +0900

    色パターンを4種類に追加した

--- LifeCell.java
+++ LifeCell.java
@@ -14,9 +14,9 @@ class LifeCell extends JButton{
 	//周囲のセル
 	private ArrayList<LifeCell> surroundings;
 	//現在の生存フラグ
-	private boolean isLiving = false;
+	private int isLiving = 0;
 	//世代更新後の生存フラグ
-	private boolean willLiving = false;
+	private int willLiving = 0;
 	/**
 	 * Constructors
 	 */
@@ -47,7 +47,7 @@ class LifeCell extends JButton{
 		this.addActionListener(
 				new ActionListener(){
 						public void actionPerformed(ActionEvent event){
-							if(button.isLiving){
+							if(button.isLiving==1){
 								button.forceKill();
 							}else{
 								button.forceSpawn();
@@ -68,66 +68,82 @@ class LifeCell extends JButton{
 	 */
 	public void generationalChange(){
 		isLiving = willLiving;
-		willLiving = false;
-		if(isLiving){
-			setBackground(Color.black);
-		}else{
-			setBackground(Color.white);			
-		}
-	}
+		willLiving = 0;
+		if(isLiving == 1){
+			setBackground(Color.yellow);
+                }
+                      else  if(isLiving == 2){
+                            setBackground(Color.red);
+                        }
+
+                      else  if(isLiving == 3){
+                            setBackground(Color.green);
+                        }
+                        
+                      else  if(isLiving == 4){
+                            setBackground(Color.blue);
+                        }
+
+
+else{
+                            setBackground(Color.white);			
+                        }
+                }
 	/**
 	 * 周りのセルを調べて、世代交代の準備
 	 */
 	public void checkSurroundings(){
 		int cnt = 0;
+int cnt1 = 0;
+int cnt2 = 0;
+int cnt3 = 0;
+int cnt4 = 0;
+int cntall =0;
 		for(LifeCell cell : surroundings){
-			if(cell.isLiving) cnt++;
+			if(cell.isLiving == 1) cnt1++;
+                        if(cell.isLiving == 2) cnt2++;
+                        if(cell.isLiving == 3) cnt3++;
+                        if(cell.isLiving == 4) cnt4++;
+                        cnt= cnt1 + cnt2 + cnt3 + cnt4;
+                        cntall= cnt1 + (2*cnt2) + (3*cnt3) + (4*cnt4);
+        
 		}
 		//誕生
-		if(!isLiving){
+		if(isLiving == 0){
 			//生きているセルが3つ
 			if(cnt == Const.BIRTH_CNT){
-				this.willLiving = true;
-			}
+                            
+                            this.willLiving = (cntall % 4)+1;
+                        }
+                        
 			return;
+                       
 		}
-		//クロージャ内で使用したいので、定数として再定義
-		if(isLiving){
-			//生存
-			if(Const.LIVING_RANGE.includes(cnt)){
-				this.willLiving = true;
-			}
-			//過疎
-			if(Const.LIVING_RANGE.lowerBound > cnt){
-				this.willLiving = false;
-			}
-			//過密
-			if(Const.LIVING_RANGE.upperBound < cnt){
-				this.willLiving = false;
-			}
-		}
-	}
+ else this.willLiving = isLiving; 
+        }
+	
 	/**
 	 * ボタンを押して生成する場合
 	 */
 	public void forceSpawn(){
-		isLiving = true;
-		this.setBackground(Color.black);
+                isLiving = 1;
+		this.setBackground(Color.yellow);
+          
 	}
 	/**
 	 * ボタンを押してセルを消す場合
 	 */
 	public void forceKill(){
-		isLiving = false;
-		this.setBackground(Color.white);
+		isLiving = 2;
+		this.setBackground(Color.red);
 	}
 	/**
 	 * 盤面初期化用
 	 */
 	public static void forceKillAll(ArrayList<LifeCell> cells){
 		for(LifeCell cell : cells){
-			cell.isLiving = false;
-			cell.willLiving = false;
+			cell.isLiving = 0;
+			cell.willLiving = 0;
 			cell.setBackground(Color.white);			
 		}
 	}
